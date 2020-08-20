6212438129b8defd9a39be624894226e2ba59c3a
Mike Hersom <mikeh@corp.ejprescott.com>
2020-08-20 16:42:44 -0400

    August 20th, Day 49 100 days of code

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,6 +38,7 @@ const ContactState = (props) => {
       },
     ],
     current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -64,24 +65,33 @@ const ContactState = (props) => {
   };
 
   // Update Crruent Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  const updateContact = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
   };
 
   //Filter Contacts
+  const filterContacts = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
